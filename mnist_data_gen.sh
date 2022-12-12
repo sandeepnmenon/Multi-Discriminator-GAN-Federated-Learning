f0d679575c89b49a8f561dda8165b9f54f3acc02
Sandeep Menon <snm6477@nyu.edu>
2022-12-12 17:20:52 -0500

    noniid tag added for nonidd data folders

--- mnist_data_gen.sh
+++ mnist_data_gen.sh
@@ -14,18 +14,18 @@ python split_dataset.py --dir_download ../current_dir --logs_dir ../logs --split
 
 ## Non iid
 
-python split_dataset.py --dir_download ../current_dir --logs_dir ../logs --split_type noniid --beta_value 0.1 --dataset mnist --num_clients 2 --result_directory ../../mnist_splits_2_01
-python split_dataset.py --dir_download ../current_dir --logs_dir ../logs --split_type noniid --beta_value 0.5 --dataset mnist --num_clients 2 --result_directory ../../mnist_splits_2_05
-python split_dataset.py --dir_download ../current_dir --logs_dir ../logs --split_type noniid --beta_value 5 --dataset mnist --num_clients 2 --result_directory ../../mnist_splits_2_5
+python split_dataset.py --dir_download ../current_dir --logs_dir ../logs --split_type noniid --beta_value 0.1 --dataset mnist --num_clients 2 --result_directory ../../mnist_splits_2_01_noniid
+python split_dataset.py --dir_download ../current_dir --logs_dir ../logs --split_type noniid --beta_value 0.5 --dataset mnist --num_clients 2 --result_directory ../../mnist_splits_2_05_noniid
+python split_dataset.py --dir_download ../current_dir --logs_dir ../logs --split_type noniid --beta_value 5 --dataset mnist --num_clients 2 --result_directory ../../mnist_splits_2_5_noniid
 
-python split_dataset.py --dir_download ../current_dir --logs_dir ../logs --split_type noniid --beta_value 0.1 --dataset mnist --num_clients 3 --result_directory ../../mnist_splits_3_01
-python split_dataset.py --dir_download ../current_dir --logs_dir ../logs --split_type noniid --beta_value 0.5 --dataset mnist --num_clients 3 --result_directory ../../mnist_splits_3_05
-python split_dataset.py --dir_download ../current_dir --logs_dir ../logs --split_type noniid --beta_value 5  --dataset mnist --num_clients 3 --result_directory ../../mnist_splits_3_5
+python split_dataset.py --dir_download ../current_dir --logs_dir ../logs --split_type noniid --beta_value 0.1 --dataset mnist --num_clients 3 --result_directory ../../mnist_splits_3_01_noniid
+python split_dataset.py --dir_download ../current_dir --logs_dir ../logs --split_type noniid --beta_value 0.5 --dataset mnist --num_clients 3 --result_directory ../../mnist_splits_3_05_noniid
+python split_dataset.py --dir_download ../current_dir --logs_dir ../logs --split_type noniid --beta_value 5  --dataset mnist --num_clients 3 --result_directory ../../mnist_splits_3_5_noniid
 
-python split_dataset.py --dir_download ../current_dir --logs_dir ../logs --split_type noniid --beta_value 0.1 --dataset mnist --num_clients 5 --result_directory ../../mnist_splits_5_01
-python split_dataset.py --dir_download ../current_dir --logs_dir ../logs --split_type noniid --beta_value 0.5 --dataset mnist --num_clients 5 --result_directory ../../mnist_splits_5_05
-python split_dataset.py --dir_download ../current_dir --logs_dir ../logs --split_type noniid --beta_value 5  --dataset mnist --num_clients 5 --result_directory ../../mnist_splits_5_5
+python split_dataset.py --dir_download ../current_dir --logs_dir ../logs --split_type noniid --beta_value 0.1 --dataset mnist --num_clients 5 --result_directory ../../mnist_splits_5_01_noniid
+python split_dataset.py --dir_download ../current_dir --logs_dir ../logs --split_type noniid --beta_value 0.5 --dataset mnist --num_clients 5 --result_directory ../../mnist_splits_5_05_noniid
+python split_dataset.py --dir_download ../current_dir --logs_dir ../logs --split_type noniid --beta_value 5  --dataset mnist --num_clients 5 --result_directory ../../mnist_splits_5_5_noniid
 
-python split_dataset.py --dir_download ../current_dir --logs_dir ../logs --split_type noniid --beta_value 0.1 --dataset mnist --num_clients 10 --result_directory ../../mnist_splits_10_01
-python split_dataset.py --dir_download ../current_dir --logs_dir ../logs --split_type noniid --beta_value 0.5 --dataset mnist --num_clients 10 --result_directory ../../mnist_splits_10_05
-python split_dataset.py --dir_download ../current_dir --logs_dir ../logs --split_type noniid --beta_value 5  --dataset mnist --num_clients 10 --result_directory ../../mnist_splits_10_5
+python split_dataset.py --dir_download ../current_dir --logs_dir ../logs --split_type noniid --beta_value 0.1 --dataset mnist --num_clients 10 --result_directory ../../mnist_splits_10_01_noniid
+python split_dataset.py --dir_download ../current_dir --logs_dir ../logs --split_type noniid --beta_value 0.5 --dataset mnist --num_clients 10 --result_directory ../../mnist_splits_10_05_noniid
+python split_dataset.py --dir_download ../current_dir --logs_dir ../logs --split_type noniid --beta_value 5  --dataset mnist --num_clients 10 --result_directory ../../mnist_splits_10_5_noniid
