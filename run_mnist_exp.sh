0ea032784c681b457963fabe74af291249edacd5
Sandeep Menon <snm6477@nyu.edu>
2022-12-12 17:06:18 -0500

    correct mnist script

--- run_mnist_exp.sh
+++ run_mnist_exp.sh
@@ -5,8 +5,7 @@ set -e
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/
 
 
-# folders=("mnist_splits_2_01" "mnist_splits_3_01" "mnist_splits_5_01") 
-folders=("mnist_splits_2_01" "mnist_splits_3_01")
+folders=("mnist_splits_2_01" "mnist_splits_3_01" "mnist_splits_5_01") 
 # fold="mnist_splits_2_01"
 echo "Starting experiments"
 
@@ -19,7 +18,7 @@ for fold in ${folders[@]}; do
     sleep 3  # Sleep to give the server enough time to start
     for i in `seq 1 $num_clients`; do
         echo "Starting client $i for ${fold}/client_${i}"
-        python ./GAN_client/GAN_Code_Flower_Client.py --client_id ${i} --dataset_path `${fold}/client_${i}` --batch_size 128 &
+        python ./GAN_client/GAN_Code_Flower_Client.py --client_id ${i} --dataset_path ${fold}/client_${i} --batch_size 128 &
     done
 done
 
