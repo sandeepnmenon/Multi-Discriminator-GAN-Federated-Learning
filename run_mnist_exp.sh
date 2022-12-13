0cd307f71603d5c0280094b107d6a9f02a942a7d
Sandeep Menon <snm6477@nyu.edu>
2022-12-13 04:00:48 -0500

    server sleept to 5 seconds

--- run_mnist_exp.sh
+++ run_mnist_exp.sh
@@ -5,7 +5,7 @@ set -e
 cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/
 
 
-folders=("mnist_splits_2_01" "mnist_splits_3_01" "mnist_splits_5_01" "mnist_splits_10_01") 
+folders=("mnist_splits_2_01_noniid" "mnist_splits_2_05_noniid" "mnist_splits_2_5_noniid") 
 # fold="mnist_splits_2_01"
 echo "Starting experiments"
 
@@ -18,7 +18,7 @@ for fold in ${folders[@]}; do
     # python split_dataset.py --dir_download ../current_dir --logs_dir ../logs --split_type iid --beta_value 0.1 --dataset mnist --num_clients ${num_clients} --result_directory ../../${fold}
 
     python Flower_Server.py --batch_size 128 --dataset_size 60000 --latent_dim_input 100 --original_dataset_path ./Original_MNIST_Dataset --device cuda:0 --num_clients ${num_clients} --experiment_name ${fold} &
-    sleep 3  # Sleep to give the server enough time to start
+    sleep 5  # Sleep to give the server enough time to start
     for i in `seq 1 $num_clients`; do
         echo "Starting client $i for ${fold}/client_${i}"
         python ./GAN_client/GAN_Code_Flower_Client.py --client_id ${i} --dataset_path ${fold}/client_${i} --batch_size 128 &
