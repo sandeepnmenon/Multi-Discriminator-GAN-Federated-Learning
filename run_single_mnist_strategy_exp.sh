e413baa91d0cbdbe05ff1f406a397f346425de06
Sandeep N Menon <snm6477@nyu.edu>
2022-12-15 17:17:56 -0500

    Training code for Basic Gan running in all clients (#1)
    * GAN in each client experiment code
    
    * added port number as an argument
    
    * evaluate_config added
    
    * total_number_of_rounds = 110
    
    * removing eval dir
    
    
    * 1. strategy experiment
    2. febn enabled by default
    
    * fedbn experiments
    
    * strategy experiment with mnist_splits_5_05_noniid
    
    * 1. cifar 10 ignores
    2. mnist strategy exp script updates
    
    * bug in strategy experiment code
    
    * updated strategy configs
    
    * sending arguments for other FL strategies

---
+++ run_single_mnist_strategy_exp.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# This script runs the MNIST experiment with the given number of clients for both generator and discriminator in same client
+set -e
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/
+
+
+folders=("mnist_splits_5_05_noniid") 
+strategies=("FedAdam" "FedYogi")
+echo "Starting experiments"
+
+for fold in ${folders[@]}; do
+    echo "Starting experiment $fold"
+    num_clients=$(cut -d'_' -f3 <<< "$fold")
+    echo "Number of clients: $num_clients"
+
+    for strat in ${strategies[@]}; do
+        python Flower_Server_basic.py --strategy_type ${strat} --port 8889 --batch_size 128 --dataset_size 60000 --latent_dim_input 100 --original_dataset_path ./Original_MNIST_Dataset --device cuda:0 --num_clients ${num_clients} --experiment_name ${fold}_basic_${strat} &
+        sleep 5  # Sleep to give the server enough time to start
+        for i in `seq 1 $num_clients`; do
+            echo "Starting client $i for ${fold}/client_${i}"
+            python ./GAN_client_basic/GAN_Code_Flower_Client.py --port 8889 --client_id ${i} --dataset_path ${fold}/client_${i} --batch_size 128 &
+        done
+        wait
+    done
+done
+
+# Enable CTRL+C to stop all background processes
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM
+# Wait for all background processes to complete
+wait
