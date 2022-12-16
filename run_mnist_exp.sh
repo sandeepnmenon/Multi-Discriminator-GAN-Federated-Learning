4bc04e3652f206fbfdc891f7ca1306f3a3cd228f
Sandeep Menon <snm6477@nyu.edu>
2022-12-16 14:32:48 -0500

    updated shell scripts

--- run_mnist_exp.sh
+++ run_mnist_exp.sh
@@ -14,9 +14,6 @@ for fold in ${folders[@]}; do
     num_clients=$(cut -d'_' -f3 <<< "$fold")
     echo "Number of clients: $num_clients"
 
-    # cd experiments_folder/MOON
-    # python split_dataset.py --dir_download ../current_dir --logs_dir ../logs --split_type iid --beta_value 0.1 --dataset mnist --num_clients ${num_clients} --result_directory ../../${fold}
-
     python Flower_Server.py --batch_size 128 --dataset_size 60000 --latent_dim_input 100 --original_dataset_path ./Original_MNIST_Dataset --device cuda:0 --num_clients ${num_clients} --experiment_name ${fold} &
     sleep 5  # Sleep to give the server enough time to start
     for i in `seq 1 $num_clients`; do
